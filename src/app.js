3f13c7b05b64f92cba58da17a56d95f0c5cb1b9a
Malik Mahmud <almalikmahmud@gmail.com>
2020-08-05 20:38:17 +0100

    Set view engine

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
+import path from 'path';
 import express from 'express';
 
 const port = process.env.PORT;
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('*', (req, res) => {
-  res.status(200).send('What it do, baybeeee!!!!');
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Running on port ${port}`));
